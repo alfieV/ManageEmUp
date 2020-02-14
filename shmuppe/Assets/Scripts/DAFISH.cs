19837f43a96ae03bf2c58fa1a6072cfdf6d8afb3
alfred vié <alfredvie@gmail.com>
2020-02-14 16:04:10 +0100

    comportement poisson et prise dégat joueur (base)

---
+++ shmuppe/Assets/Scripts/DAFISH.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DAFISH : MonoBehaviour
+{
+    [SerializeField]
+    float speed = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, new Vector2(transform.position.x,transform.position.y -1), speed * Time.deltaTime);
+    }
+}
