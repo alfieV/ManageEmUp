3d55380fbddc2630f34460d97f8e1031f6e21d6f
alfred vié <alfredvie@gmail.com>
2020-02-14 16:22:35 +0100

    pause

---
+++ shmuppe/Assets/Scripts/Pause.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    bool paused = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!paused)
+            {
+                Time.timeScale = 0;
+                transform.GetChild(0).gameObject.SetActive(true);
+                paused = true;
+            }
+            else if (paused)
+            {
+                Time.timeScale = 1;
+                transform.GetChild(0).gameObject.SetActive(false);
+                paused = false;
+            }
+        }
+    }
+}
