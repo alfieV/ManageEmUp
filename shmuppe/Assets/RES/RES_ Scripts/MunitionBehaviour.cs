98826921eed5991caf456255df7a4188b7d70216
Rémi Sécher <r.secher@rubika-edu.com>
2020-02-14 15:52:32 +0100

    PlayerShooting v1 end

---
+++ shmuppe/Assets/RES/RES_ Scripts/MunitionBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MunitionBehaviour : MonoBehaviour
+{
+    [SerializeField] float speed;
+    Rigidbody2D projectileRgb;
+
+
+    void Start()
+    {
+        projectileRgb = GetComponent<Rigidbody2D>();
+        FireBullet();
+        StartCoroutine(TempDestroyer());
+    }
+
+    void FireBullet()
+    {
+        Debug.Log(transform.forward);
+        projectileRgb.velocity = transform.right * speed; 
+    }
+
+
+    IEnumerator TempDestroyer()
+    {
+        yield return new WaitForSeconds(5f);
+        Destroy(gameObject);
+    }
+
+}
