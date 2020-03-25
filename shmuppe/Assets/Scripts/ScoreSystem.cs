e849da57d8225b23a44b945204a269f31579adb6
A.V <alfredvie@gmail.com>
2020-03-25 14:54:56 +0100

    added gamemanager prefab

---
+++ shmuppe/Assets/Scripts/ScoreSystem.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreSystem : MonoBehaviour
+{
+    private int score = 0;
+
+    public int Score
+    {
+        get { return score; }
+        set { score = score + value; }
+    }
+
+    public void Reset()
+    {
+        score = 0;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
