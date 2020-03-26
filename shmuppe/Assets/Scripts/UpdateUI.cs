17c64fc5ec1a8d6e5a010a2a96be0646366103d8
A.V <alfredvie@gmail.com>
2020-03-26 10:45:43 +0100

    fix score

--- shmuppe/Assets/Scripts/UpdateUI.cs
+++ shmuppe/Assets/Scripts/UpdateUI.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class UpdateUI : MonoBehaviour
 {
@@ -9,34 +10,50 @@ public class UpdateUI : MonoBehaviour
     private int score;
     private int hp;
     PlayerHealthSystem plhp;
+    private bool isAtributed = false;
     string heart = "♥";
     string life;
     // Start is called before the first frame update
     void Start()
     {
         sco = GameObject.Find("GameManager").GetComponent<ScoreSystem>();
-        plhp = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHealthSystem>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        score = sco.Score;
-        transform.GetChild(0).GetComponent<Text>().text = "Score" + score.ToString();
-        hp = plhp.currentHealth;
-        life = string.Empty;
-        
-        if (hp > 99)
+        if (SceneManager.GetActiveScene().buildIndex == 0)
         {
-            life = "+99♥";
-        }
-        else
-        {
-            for (int i = 0; i < hp; i++)
+            if (!isAtributed)
             {
-                life += heart;
+                try
+                {
+                    plhp = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHealthSystem>();
+                    isAtributed = true;
+                }
+                catch
+                {
+                    Debug.Log("waitforstart");
+                }
             }
+
+            score = sco.Score;
+            transform.GetChild(0).GetComponent<Text>().text = "Score : " + score.ToString();
+            hp = plhp.currentHealth;
+            life = string.Empty;
+
+            if (hp > 99)
+            {
+                life = "+99♥";
+            }
+            else
+            {
+                for (int i = 0; i < hp; i++)
+                {
+                    life += heart;
+                }
+            }
+            transform.GetChild(1).GetComponent<Text>().text = life;
         }
-        transform.GetChild(1).GetComponent<Text>().text = life;
     }
 }
