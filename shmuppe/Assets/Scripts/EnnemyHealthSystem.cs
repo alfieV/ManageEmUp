b6fb773a48369474ab3187f9d5722b8498558bd3
A.V <alfredvie@gmail.com>
2020-03-26 10:55:04 +0100

    enemy health system

---
+++ shmuppe/Assets/Scripts/EnnemyHealthSystem.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnnemyHealthSystem : MonoBehaviour
+{
+    [SerializeField] int startHealth = 100;
+    private int currentHealth;
+    private ScoreSystem scosy;
+
+    [Range(1,100)]public int points;
+
+    private void Start()
+    {
+        currentHealth = startHealth;
+        scosy = GameObject.Find("GameManager").GetComponent<ScoreSystem>();
+    }
+
+    private void Update()
+    {
+        if (currentHealth <= 0)
+        {
+            Dead();
+        }
+    }
+
+    public void TakeDmg(int dmg)
+    {
+        currentHealth -= dmg;
+
+        if (currentHealth <= 0)
+        {
+            Dead();
+        }
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Fish")
+        {
+            TakeDmg(1);
+        }
+    }
+
+    void Dead()
+    {
+        scosy.Score = points;
+        Destroy(gameObject);
+    }
+}
